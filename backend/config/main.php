da095f55b520dcd928a910c9059e93d87e2e0234
padidsam <padidsam@gmail.com>
2015-09-30 14:48:56 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- backend/config/main.php
+++ backend/config/main.php
@@ -1,9 +1,7 @@
 <?php
+
 $params = array_merge(
-    require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
+        require(__DIR__ . '/../../common/config/params.php'), require(__DIR__ . '/../../common/config/params-local.php'), require(__DIR__ . '/params.php'), require(__DIR__ . '/params-local.php')
 );
 
 return [
@@ -17,6 +15,15 @@ return [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
         ],
+        
+//        'view' => [
+//         'theme' => [
+//             'pathMap' => [
+//                '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
+//             ],
+//         ],
+//    ],
+        
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
--- common/config/main.php
+++ common/config/main.php
@@ -1,4 +1,5 @@
 <?php
+
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
@@ -6,16 +7,34 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
         'view' => [
-         'theme' => [
-             'pathMap' => [
-                '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
-             ],
-         ],
+            'theme' => [
+                'pathMap' => [
+                    '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
+                ],
+            ],
+        ],
+//        'user' => [
+//            'class' => 'webvimark\modules\UserManagement\components\UserConfig',
+//            // Comment this if you don't want to record user logins
+//            'on afterLogin' => function($event) {
+//                \webvimark\modules\UserManagement\models\UserVisitLog::newVisitor($event->identity->id);
+//            }
+//        ],
     ],
-    ],
-    'modules' =>[
+    'modules' => [
         'gridview' => [
-            'class'=> '\kartik\grid\Module'
-        ]
+            'class' => '\kartik\grid\Module'
+        ],
+//        'user-management' => [
+//            'class' => 'webvimark\modules\UserManagement\UserManagementModule',
+//            // 'enableRegistration' => true,
+//            // Here you can set your handler to change layout for any controller or action
+//            // Tip: you can use this event in any module
+//            'on beforeAction' => function(yii\base\ActionEvent $event) {
+//                if ($event->action->uniqueId == 'user-management/auth/login') {
+//                    $event->action->controller->layout = 'loginLayout.php';
+//                }
+//            },
+//        ],
     ],
 ];

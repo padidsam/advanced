054bce4f3bb46344867d32c3b3513a5f5f41fc88
padidsam <padidsam@gmail.com>
2016-02-02 11:14:00 +0700

    update yii advance

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -13,7 +13,7 @@ $str = 'padidsam11111';
 
 if (md5($str) === 'a5bca4f99d7fc0c158f1bd8028e5011c') {
     echo "Would you like a green or red padidsam11111?";
-    echo "<br>ทดสอบอัพgithob"
+    echo "<br>ทดสอบอัพgithob";
 }
 
 ?>
